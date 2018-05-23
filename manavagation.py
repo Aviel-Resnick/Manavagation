fe046b93648a5ca2a447fc394a486745dbb96d42
Aviel Resnick <aviel.resnick@gmail.com>
2018-05-23 13:30:53 -0400

    V2
    Updated to find the average step length to a random point

--- manavagation.py
+++ manavagation.py
@@ -2,46 +2,63 @@ import turtle
 import random
 
 turtle.screensize(canvwidth=500, canvheight=500)
-turtle.speed("fastest")
-turtle.hideturtle()
-#turtle.bgcolor("black")
-turtle.pensize(5)
-turtle.penup()
 
-#targetX = input("Target X: ")
-#targetY = input("Target Y: ")
-targetX = 200
-targetY = 200
-prevX = 0
-prevY = 0
+def main():
+    value = 0
+    turtle.setpos(0, 0)
+    iteration = 1
 
-turtle.color("red")
-turtle.setpos(int(targetX), int(targetY))
-turtle.dot()
+    while True:
+        targetX = random.randint(-400, 400)
+        targetY = random.randint(-400, 400)
+        value += find(targetX, targetY)
+        print("Current Average: " + str(value / iteration) + " Iteration: " + str(iteration))
+        iteration += 1
+        turtle.reset()
 
-turtle.color("blue")
+    print("Average: " + str(value/100))
 
-turtle.setpos(0,0)
-turtle.dot()
+def find(targetX, targetY):
+    turtle.speed("fastest")
+    turtle.hideturtle()
+    turtle.pensize(5)
+    
+    prevX = 0
+    prevY = 0
 
-turtle.color("black")
+    turtle.penup()
+    turtle.color("red")
+    turtle.setpos(int(targetX), int(targetY))
+    turtle.dot()
 
-while (abs(turtle.xcor() - int(targetX)) > 25 or abs(turtle.ycor() - int(targetY)) > 25):
-    turtle.pendown()
-    angle = random.randint(0,361)
-    distance = random.randint(0,100)
+    turtle.color("blue")
 
-    prevX = turtle.xcor()
-    prevY = turtle.ycor()
+    turtle.setpos(0,0)
+    turtle.dot()
 
-    turtle.setheading(angle)
-    turtle.forward(distance)
+    turtle.color("black")
 
-    if(turtle.xcor() < -400 or turtle.xcor() > 400 or turtle.ycor() < -400 or turtle.ycor() > 400):
-        turtle.color("white")
-        turtle.setpos(prevX, prevY)
-        turtle.color("black")
+    moves = 0
 
-print("Found.")
+    while (abs(turtle.xcor() - int(targetX)) > 25 or abs(turtle.ycor() - int(targetY)) > 25):
+        moves += 1
+        turtle.pendown()
+        angle = random.randint(0,361)
+        distance = random.randint(0,100)
+
+        prevX = turtle.xcor()
+        prevY = turtle.ycor()
+
+        turtle.setheading(angle)
+        turtle.forward(distance)
+
+        if(turtle.xcor() < -400 or turtle.xcor() > 400 or turtle.ycor() < -400 or turtle.ycor() > 400):
+            turtle.color("white")
+            turtle.setpos(prevX, prevY)
+            turtle.color("black")
+    
+    return moves
+
+main()
 
 turtle.done()
